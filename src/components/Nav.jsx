d4bd6f18d44a1c71f47f511d334b96a779163c5c
Arnav-lunatic <arnav4c@gmail.com>
2024-11-07 01:54:58 +0530

    load logo image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { switchTheme } from '../features/themeSlice'
+import marvel from '/src/assets/marvel.png'
 
 import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 
@@ -21,7 +22,7 @@ export default function Nav() {
     
     return (
         <div className='max-w-[1680px] w-full my-2 px-2 flex justify-between bg-black bg-opacity-20 backdrop-blur-lg rounded-lg z-50'>
-            <img className='w-20' src="./src/assets/marvel.png" alt="" />
+            <img className='w-20' src={marvel} alt="" />
             <button
                 className='w-20'
                 onClick={() => dispatch(switchTheme())}
