6916ca2dd5a4974c376ab8d6bf89f50cb3ba6d22
Arnav-lunatic <arnav4c@gmail.com>
2025-01-27 18:32:15 +0530

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Analytics } from "@vercel/analytics/react"
 function App() {
 
 	return (
-		<div className="flex gap-4 flex-col md:flex-row h-full max-w-[1680px] w-full relative pt-20">
+		<div className="flex m-auto gap-4 flex-col md:flex-row h-full md:h-[100dvh] max-w-[1680px] w-full relative pt-20 pb-2">
 			<Nav />
 			<div className="flex-grow">
 				<Outlet />
